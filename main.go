a8755daac2f05d980a7b3be106709658e180754b
Denis Riabtsev <denischool119@gmail.com>
2023-12-14 15:13:55 +0200

    Setup key endpoints

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"redsunsetbackend/requests"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/verifyPhoto", requests.HandleVerifyPhoto)
+	e.GET("/signerInclusionProof", requests.HandleSignerInclusionProof)
+	e.GET("/providerMerkleRoot", requests.HandleSignerInclusionProof)
+	e.GET("/providerList", requests.HandleSignerInclusionProof)
+	e.POST("/updateMerkleRoot", requests.HandleUpdateMerkleRoot)
+
+}
---
+++ requests/requests.go
@@ -0,0 +1,25 @@
+package requests
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+func HandleVerifyPhoto(context echo.Context) error {
+	return nil
+}
+
+func HandleSignerInclusionProof(context echo.Context) error {
+	return nil
+}
+
+func HandleProviderMerkleRoot(context echo.Context) error {
+	return nil
+}
+
+func HandleProviderList(context echo.Context) error {
+	return nil
+}
+
+func HandleUpdateMerkleRoot(context echo.Context) error {
+	return nil
+}
