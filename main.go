b29ee3ef5fccfc87cc9ee5e592de01f537b227a4
Denis Riabtsev <denischool119@gmail.com>
2023-12-21 14:43:03 +0200

    VerifyPhoto changes Get to Post

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	requests.MerkleTree = merkletree.NewMerkleTree(1)
 	requests.MerkleTree.BuildMerkleTreeMock()
 
-	e.GET("/verifyPhoto", requests.HandleVerifyPhoto)
+	e.POST("/verifyPhoto", requests.HandleVerifyPhoto)
 	e.GET("/providerInclusionProof/:leafHash", requests.HandleProviderInclusionProof)
 	e.GET("/providerMerkleRoot", requests.HandleProviderMerkleRoot)
 	e.GET("/providerList", requests.HandleProviderList)
